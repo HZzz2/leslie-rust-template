330597f289e1246aa239e6d9d1f1f34b50bf0350
HZzz2 <hzzz2yb@gmail.com>
2024-11-26 01:46:37 +0800

    完善模板

--- toolchain.sh
+++ toolchain.sh
@@ -1,4 +1,12 @@
 apt update
-sudo apt install mingw-w64
+sudo apt install mingw-w64 -y
 rustup target add x86_64-pc-windows-gnu
 alias cbw='cargo build -r --target x86_64-pc-windows-gnu'
+
+## extensions
+# rust-analyzer
+# Even Better TOML
+# GitHub Actions
+# Error Lens
+# Night Owl
+# vscode-icons
